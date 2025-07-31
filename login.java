82c4f82d2b0d4b18da949338c42eb4b50763c097
vamsi <vamsi@mail.com>
2025-07-31 10:58:58 +0530

    register java added

--- login.java
+++ login.java
@@ -1,3 +1,4 @@
 login page should have user name input box & password input box
 login page shoud have reset passworood
 this page should have logo of facebook in the left side
+login page should contain register option
